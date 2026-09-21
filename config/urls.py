951bb5ef86b354e2d4c990eeeca1b8bec92a646a
myarilove1881989-maker <myarilove1881989@gmail.com>
2026-09-21 16:13:21 +0200

    Ajouter la route d export statistique

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,10 @@ from django.urls import path, include
 # ✅ IMPORT COMPLET (corrige ton erreur)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.admin_dashboard import superadmin_dashboard
+from core.admin_exports import export_all_statistics
 
 urlpatterns = [
+    path('admin/export-statistics/', admin.site.admin_view(export_all_statistics), name='admin-export-statistics'),
     path('admin/super-dashboard/', admin.site.admin_view(superadmin_dashboard), name='superadmin-dashboard'),
     path('admin/', admin.site.urls),
 
